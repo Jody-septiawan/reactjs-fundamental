47e4491d85ea8ba0ef66988491d874538ae015a3
Jody Septiawan <jody@192.168.1.12>
2021-09-21 10:35:39 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import EmbedExpression from "./embedExpression";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import EmbedExpression from './embedExpression';
 
 ReactDOM.render(
   <React.StrictMode>
     <EmbedExpression />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
