8369b8b791d3af61075d964c3092977de42cc787
Jody Septiawan <jody@192.168.1.12>
2021-09-21 10:16:54 +0700

    Update

--- src/component.js
+++ src/component.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React from 'react';
+import Header from './components/header';
 
 function Component() {
   return (
     //   Code Here
-    <div></div>
+    <div>
+      <Header />
+      <Content />
+    </div>
   );
 }
 
 // Create a new component here
+function Content() {
+  return (
+    <div>
+      <h1>This is A content</h1>
+      <button>Click me</button>
+    </div>
+  );
+}
 
 export default Component;
--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,12 @@
 // Code Here
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>This is Header</h1>
+    </div>
+  );
+}
+
+export default Header;
