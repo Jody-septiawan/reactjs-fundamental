99a187ccd47a4f7424808691afbf291e07e94936
Jody Septiawan <jody@192.168.1.12>
2021-09-21 11:02:28 +0700

    Update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,17 @@
 // code Here
+import React from 'react';
+
+function List(props) {
+  //   const { listData, color } = props;
+
+  console.log(props);
+
+  return (
+    <div>
+      <h1>{props.listData}</h1>
+      <h5>{props.color}</h5>
+    </div>
+  );
+}
+
+export default List;
--- src/props.js
+++ src/props.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react';
 
 //Import Components
+import List from './components/list';
 
 function Props() {
+  const car = {
+    listData: 'Ferrari',
+    color: 'red',
+  };
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>On the image element using the default props, namely src</p>
+      <img
+        src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNDE2Njh8MHwxfHNlYXJjaHwxfHxjYXJ8ZW58MHx8fHwxNjMyMTk2MTg4&ixlib=rb-1.2.1&q=80&w=400"
+        alt="image"
+      />
+
+      <List listData="MBW" color="red" />
+      <List listData="Mercedes-Benz" color="black" />
+      <List listData="Lamborghini" color="blue" />
+      {/* <List car={car} /> */}
+    </div>
   );
 }
 
