4d062379c4ae0b533efd22ffeadc533f4ef853f5
Jody Septiawan <jody@192.168.1.12>
2021-09-21 11:23:31 +0700

    Update

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //Create Component PrivatePage
+function PrivatePage(props) {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={props.logout}>Logout</button>
+    </div>
+  );
+}
 
 //Create Component GuestPage
+function GuestPage(props) {
+  return (
+    <div>
+      <h1>Please sign in</h1>
+      <button onClick={props.login}>Login</button>
+    </div>
+  );
+}
 
 function ConditionRenderin() {
   //init State
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  console.log(isLoggedin);
+
+  // if (isLoggedin) {
+  //   return <PrivatePage logout={() => setIsLoggedin(!isLoggedin)} />;
+  // } else {
+  //   return <GuestPage login={() => setIsLoggedin(!isLoggedin)} />;
+  // }
+
   return (
     // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>
+      {/* conditional logic rendering */}
+      {isLoggedin ? (
+        <PrivatePage logout={() => setIsLoggedin(!isLoggedin)} />
+      ) : (
+        <GuestPage login={() => setIsLoggedin(!isLoggedin)} />
+      )}
+    </div>
   );
 }
 
