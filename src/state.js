85564f50cfd34cd49cfbd5993222917a49646228
Jody Septiawan <jody@192.168.1.12>
2021-09-21 11:13:13 +0700

    Update

--- src/state.js
+++ src/state.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function State() {
   //init State
+  const [counter, setCounter] = useState(0);
+  const [greeting, setGreeting] = useState('');
 
   //Create Function Add
+  function Add() {
+    return setCounter(counter + 1);
+  }
 
   //Create Function Less
+  function Less() {
+    return setCounter(counter - 1);
+  }
+
+  function Greeting() {
+    setGreeting('Halo batch 26');
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        If you click the add button it wall increase by one, vice versa if you
+        click the less button it will descrease by one
+      </p>
+
+      <h2>{counter}</h2>
+
+      <h1>{greeting}</h1>
+
+      <button onClick={Add}>Add</button>
+      <button onClick={Less}>Less</button>
+      <button onClick={Greeting}>Greeting</button>
+    </div>
   );
 }
 
